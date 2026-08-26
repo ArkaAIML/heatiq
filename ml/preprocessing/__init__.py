0e127655da7381c4cd2ef4593ed1c33c9f0c257e
Arka Banerjee <arkabanerjee2024@gmail.com>
2026-08-27 00:27:08 +0530

    feat: add daily temporal feature engineering

--- ml/preprocessing/__init__.py
+++ ml/preprocessing/__init__.py
@@ -7,8 +7,16 @@ from ml.preprocessing.clean import (
     validate_canonical_weather,
     validate_era5_inputs,
 )
+from ml.preprocessing.features import (
+    add_daily_temporal_features,
+    aggregate_daily_weather,
+    build_daily_feature_frame,
+)
 
 __all__ = [
+    "add_daily_temporal_features",
+    "aggregate_daily_weather",
+    "build_daily_feature_frame",
     "derive_canonical_weather",
     "load_era5_files",
     "select_nearest_point",
